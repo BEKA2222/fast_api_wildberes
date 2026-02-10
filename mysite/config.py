12ebba7580c51533dc8cc42ab623b41de5e07cb7
BEKA2222 <hiro21874@gmail.com>
2026-02-11 05:06:44 +0600

    .env

--- mysite/config.py
+++ mysite/config.py
@@ -1,4 +1,8 @@
-SECRET_KEY = '068d1257514394a4af5d9ed9722f1eca57634210cc89a7e2c22a46e862d26053'
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 ACCESS_EXPIRE_TOKEN = 60
 REFRESH_EXPIRE_TOKEN = 3
